8cb6b52a3a0d84b5b641147155544eb65e9a9910
Rafał Hirsz <hirszdesign@gmail.com>
2012-03-13 01:03:10 +0100

    Obsługa ukrywania plików w menu poprzez dopisanie ".hidden" na końcu nazwy pliku.

--- lib/toc.rb
+++ lib/toc.rb
@@ -9,7 +9,7 @@ class String
 end
 
 def toc(directory)
-    files = Dir.glob("content/#{directory}/*")
+    files = Dir.glob("content/#{directory}/*").reject { |f| f =~ /.hidden$/ }
 
     output = "<dt>#{directory.deurlize}</dt>"
     output << %Q{<dd><ul class="notes">}
